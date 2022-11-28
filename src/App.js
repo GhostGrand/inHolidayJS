aee4057c67c45bba768cd9b0c54d6b096ce1ae7a
SedovAlexandr <sedovaleksandr808@gmail.com>
2022-11-28 19:35:43 +0300

    добавлена страница с помощью

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Form from "./Components/AuthorisationRegistration/Forms/Form";
 import Registation from "./Components/AuthorisationRegistration/Registation";
 import Authorisation from "./Components/AuthorisationRegistration/Authorisation";
+import Help from "./Components/Help";
 
 function App() {
   return (
     <div className="App">
-      <Registation></Registation>
-      <Authorisation></Authorisation>
+      {/* <Registation></Registation> */}
+      {/* <Authorisation></Authorisation> */}
+      <Help></Help>
     </div>
   );
 }
---
+++ src/Components/Help.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+export default function Help() {
+  return (
+      <div className = "Help">
+        <p className = "HelpHead">Помощь</p>
+        <p className = "HelpBody">Если возникли проблемы с выбором или оплатой шаблона, свяжитесь с тех поддержкой:</p>
+        <p>По почте: maximzimin228@yandex.ru</p>
+        <p>По телефону: +796106862112</p>
+      </div>
+  );
+}
