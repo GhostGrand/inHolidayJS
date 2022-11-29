31f38abd49aed4c9e50f33f8864d9b9b80d0dc5e
Sergey <wer113wer@yandex.ru>
2022-11-29 21:16:56 +0300

    Создан подвал

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Form from "./Components/AuthorisationRegistration/Forms/Form";
 import Registation from "./Components/AuthorisationRegistration/Registation";
 import Authorisation from "./Components/AuthorisationRegistration/Authorisation";
 import Header from "./Components/Header/Header";
 import Help from "./Components/Help";
 import AboutUs from "./Components/AboutUs/AboutUs";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <AboutUs></AboutUs>
-      {/* <Header></Header> */}
-      {/* <Registation></Registation> */}
-      {/* <Authorisation></Authorisation> */}
+      {/* <AboutUs></AboutUs> */}
+      <Header></Header>
+      <Registation></Registation>
+      <Authorisation></Authorisation>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footerbar'>
+        <div className='container'>
+            <div className='footer-wrap'>
+                <ul className='footerbar-menu'>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
