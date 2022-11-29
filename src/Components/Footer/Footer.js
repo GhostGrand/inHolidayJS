ae977b22133eed8c9ef25bbda1dd945e4d032e4a
Sergey <wer113wer@yandex.ru>
2022-11-30 00:22:51 +0300

    Написаны стили подвала

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,8 +6,26 @@ export default function Footer() {
         <div className='container'>
             <div className='footer-wrap'>
                 <ul className='footerbar-menu'>
-                    
+                    <li><a href="#" className="navbar-brand">inholiday</a></li>
+                    <li><a className='footer-li' href="">Все права принадлежат четырем студентам ПрИн-367 и ВолгГТУ</a></li>
+                    <li><a className='footer-li' href="">+79610686211</a></li>
+                    <li><a className='footer-li' href=""></a></li>
                 </ul>
+
+                <ul className='footerbar-menu'>
+                    <li><a href="#" className="navbar-brand">Продукт</a></li>
+                    <li><a className='footer-li' href="">Личный кабинет</a></li>
+                    <li><a className='footer-li' href="">Шаблоны</a></li>
+                    <li><a className='footer-li' href="">Индивидуальное приглашение</a></li>
+                </ul>
+
+                <ul className='footerbar-menu'>
+                    <li><a href="#" className="navbar-brand">Проект</a></li>
+                    <li><a className='footer-li' href="">Как все работает</a></li>
+                    <li><a className='footer-li' href="">О нас</a></li>
+                    <li><a className='footer-li' href="">Главный баннер</a></li>
+                </ul>
+                
             </div>
         </div>
     </div>
