2fc8672915e596712bc635f2018ac66100356a0c
Sergey <wer113wer@yandex.ru>
2022-11-29 13:38:56 +0300

    Создана шапка и написаны стили

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,19 +2,46 @@ import React from "react";
 import "../../App.css";
 
 export default function Header() {
-  return <div className="Header">
-    <p>InHoliday</p>
-    <ul className="header">
-      <li>
-        <p>Как все работает</p>
-      </li>
-      <li>
-        <p>Помощь</p>
-      </li>
-      
-        <p>Шаблоны</p>
-        <p>О нас</p>
-      
-    </ul>
-  </div>;
+  return <header>
+    {/* <div className="header__container _container">
+      <a href=""><b>inholiday</b></a>
+      <nav className="header__menu menu">
+        <ul className="menu__list">
+          <li className="menu__item">
+            <a href="">как все работает</a>
+          </li>
+          <li className="menu__item">
+            <a href="">помощь</a>
+          </li>
+          <li className="menu__item">
+            <a href="">шаблоны</a>
+          </li>
+          <li className="menu__item">
+            <a href="">о нас</a>
+          </li>
+          <li>
+          <button className="menu__button">Зарегистрироваться</button>
+          </li>
+        </ul>
+        
+      </nav>
+    </div> */}
+    <nav className="navbar">
+      <div className="container">
+        <a href="#" className="navbar-brand">inholiday</a>
+        <div className="navbar-wrap">
+          <ul className="navbar-menu">
+            <li><a href="#">Как все работает</a></li>
+            <li><a href="#">Помощь</a></li>
+            <li><a href="#">Шаблоны</a></li>
+            <li><a href="#">О нас</a></li>
+            
+          </ul>
+          <a className="authreg">Логин|Регистрация</a>
+        </div>
+        
+      </div>
+    </nav>
+    
+  </header>;
 }
