1397c8a0e98bf71b88b53819400bfb0fca834e21
SedovAlexandr <sedovaleksandr808@gmail.com>
2022-12-10 21:50:34 +0300

    сделан компонент InfoOrder

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import AboutUs from "./Components/AboutUs/AboutUs";
 import Footer from "./Components/Footer/Footer";
 import TemplateOrderForm from "./Components/TemplateOrderForm/TemplateOrderForm";
 import PersonalAccount from "./Components/PersonalAccount/PersonalAccount";
-
+import InfoOrder from "./Components/InfoOrder/InfoOrder";
 
 function App() {
   return (
@@ -18,7 +18,8 @@ function App() {
       <Authorisation></Authorisation> */}
       {/* <AboutUs></AboutUs> */}
       {/* <TemplateOrderForm></TemplateOrderForm> */}
-      <PersonalAccount></PersonalAccount>
+      {/* <PersonalAccount></PersonalAccount> */}
+      <InfoOrder></InfoOrder>
       <Footer></Footer>
     </div>
   );
