a1e9b422b14db6174da9327bb7731db52a76b49d
Liksim <maksimzimin2020@yandex.ru>
2022-12-12 21:53:01 +0300

    Доработана регистрация

--- src/Components/AuthorisationRegistration/Registation.js
+++ src/Components/AuthorisationRegistration/Registation.js
@@ -8,6 +8,7 @@ export default function Registation() {
 
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   function onSubmit(event) {
     event.preventDefault();
@@ -22,26 +23,33 @@ export default function Registation() {
         name : formValue.name,
         email: formValue.email,
         password: formValue.password,
-        phoneNumber: formValue.phoneNumber,
-        otherCommunication: formValue.therCommunication
+        phoneNumber: formValue.phoneNumber == "" ? null : formValue.phoneNumber,
+        otherCommunication: formValue.therCommunication == "" ? null : formValue.therCommunication
       })
     })
-    .then(response => response.json())
+    .then(response => {
+      if(response.status == 400) {
+        setIsError(true)
+        setError("Неверно введён номер телефона")
+      }
+      else if(response.status == 401) {
+        setIsError(true)
+        setError("Что-то не так с серваком (401)")
+      }
+      else if(response.status == 403) {
+        setIsError(true)
+        setError("Вы уже авторизованы")
+      } 
+      else if(response.status == 409) {
+        setIsError(true)
+        setError("Пользователь с таким email уже существует")
+      }
+      else if(response.status == 500) {
+        setIsError(true)
+        setError("Что-то не так с серваком (500)")
+      }
+    })
     .then(response => console.log(JSON.stringify(response)))
-
-    /*.then(response => {
-      if(response.status == 200) {
-        response.json()
-      }
-      else {
-        response.json()
-      }
-    })*/
-    /*.then(
-      (result) => {
-        if(response.status)
-      }
-    )*/;
   }
 
   function handleInput(event) {
@@ -57,61 +65,60 @@ export default function Registation() {
         <p className="header_reg">Регистрация</p>
         <form onSubmit={onSubmit}>
           <ul>
-          <li>
-            <input 
-              className="form"
-              type="text" 
-              name="name" 
-              placeholder="ФИО" 
-              value={formValue.name.value}
-              onChange={handleInput}
-            />
-            <input
-              className="form"
-              type="email"
-              name="email"
-              placeholder="EMAIL"
-              value={formValue.email.value}
-              onChange={handleInput}
-            />
-            <input
-              className="form"
-              type="password"
-              name="password"
-              placeholder="ПАРОЛЬ"
-              value={formValue.password.value}
-              onChange={handleInput}
-            />
-          </li>
+            <li>
+              <input 
+                className="form"
+                type="text" 
+                name="name" 
+                placeholder="ФИО" 
+                value={formValue.name.value}
+                onChange={handleInput}
+              />
+              <input
+                className="form"
+                type="email"
+                name="email"
+                placeholder="EMAIL"
+                value={formValue.email.value}
+                onChange={handleInput}
+              />
+              <input
+                className="form"
+                type="password"
+                name="password"
+                placeholder="ПАРОЛЬ"
+                value={formValue.password.value}
+                onChange={handleInput}
+              />
+            </li>
 
-          <li>
-            <input
-              className="form"
-              type="number"
-              name="phone"
-              placeholder="ТЕЛЕФОН"
-              value={formValue.phoneNumber == null ? "" : formValue.phoneNumber.value}
-              onChange={handleInput}
-            />
-          </li>
-          <li>
-            <input
-              className="contacts_form"
-              type="text"
-              name="text"
-              placeholder="Иные контакты"
-              value={formValue.otherCommunication == null ? "" : formValue.otherCommunication.value}
-              onChange={handleInput}
-            />
-          </li>
+            <li>
+              <input
+                className="form"
+                type="number"
+                name="phoneNumber"
+                placeholder="ТЕЛЕФОН"
+                value={formValue.phoneNumber}
+                onChange={handleInput}
+              />
+            </li>
+            <li>
+              <input
+                className="contacts_form"
+                type="text"
+                name="otherCommunication"
+                placeholder="Иные контакты"
+                value={formValue.otherCommunication}
+                onChange={handleInput}
+              />
+            </li>
+          </ul>
+
+          <div style={{visibility: isError ? 'visible' : 'hidden' }}>
+            Ошибка: {error}.
+          </div>
           <button className="button" type="submit">Зарегистрироваться</button>
-        </ul>
         </form>
-        {/*<div visibility={false}>
-          Ошибка {error.message}
-        </div>*/}
-
-
       </div>
     </div>
   );
