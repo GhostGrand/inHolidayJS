4ccf1118f8d4a7c1804cb585f3ee3aa7e093d472
SedovAlexandr <sedovaleksandr808@gmail.com>
2022-12-23 15:06:53 +0300

    удалил фото профиля и исправил позиционирование имени заказа

--- src/Components/PersonalAccount/PersonalAccount.js
+++ src/Components/PersonalAccount/PersonalAccount.js
@@ -4,25 +4,27 @@ import Rectangle from "./Rectangle 367.png"
 
 export default function PersonalAccount() {
   return (
+    // <div className="container">
       <div className = "personalAccount">
         <p className="PAHead">личний кабинет</p>
       
         <div className="MyOrders">
             <p className="MyOrdersHead">Мои заказы</p>
             <div className="order">
-              <p className="orderName">#1<br/>свадьба</p>
+              <p className="orderName">#1<br/>свадьбаe fd grgehet  rge</p>
             </div>
             <div className="order">
-              <p className="orderName">#2<br/>свадьба</p>
+              <p className="orderName">#2<br/>свадьба ghkvbkhbkbk</p>
             </div>
             <div className="order">
               <p className="orderName">#3<br/>свадьба</p>
             </div>
+            <div className="space"></div>
         </div>
       
         <div className="Profile">
-            <img src = {Rectangle}/>
-            <p className="fio">Пушкин Александр Сергеевич</p>
+            {/* <img src = {Rectangle}/> */}
+            <p className="fio">Пушкин </p>
             <p className="number">+88005553535</p>
             <p className="mail">Kurisumkaise0@yandex.ru</p>
             <p>Иные контакты:</p>
@@ -31,5 +33,6 @@ export default function PersonalAccount() {
             <button id="btn1" className="button">Редактировать</button>
         </div>
       </div>
+    // </div>
   );
 }
