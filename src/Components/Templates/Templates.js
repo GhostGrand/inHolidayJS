7fd2be48e75e081879667b07e0d9c515a1b40d62
Sergey <wer113wer@yandex.ru>
2022-12-25 00:49:32 +0300

    Изменено название категории в шаблонах

--- src/Components/Templates/Templates.js
+++ src/Components/Templates/Templates.js
@@ -56,7 +56,7 @@ export default function Templates() {
                     </div>
                     <div className="checkbox_element">
                         <input type="checkbox" name="" id="" />
-                        <p className="checkbox-sub-text">Праздники</p>
+                        <p className="checkbox-sub-text">Другое</p>
                     </div>
                 </div>
                 
