8d4aa18e5e7b3e7ca7bf269024a3c5c07fb27b4d
SedovAlexandr <sedovaleksandr808@gmail.com>
2022-12-25 10:40:35 +0300

    добавил отступы в списке гостей и увеличил имя пользователя

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         {/* <AboutUs></AboutUs> */}
         {/* <TemplateOrderForm></TemplateOrderForm> */}
         {/* <PersonalAccount></PersonalAccount> */}
-        {/*<InfoOrder></InfoOrder>*/}
+        {/* <InfoOrder></InfoOrder> */}
         <Footer></Footer>
       </div>
     </Router>
